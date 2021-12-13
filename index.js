20202b383c64e988b5f953c7185af4dc16c5d17d
Mradul Gupta <93073227+mradul-gpt@users.noreply.github.com>
2021-12-13 11:49:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+//Genrating Random Score
+
+var score1=Math.floor(Math.random()*7);
+var score2=Math.floor(Math.random()*7);
+
+//Genrating Results
+
+if(score1>score2){
+    document.getElementsByTagName("h1")[0].innerHTML="Player 1 Wins !";
+    document.getElementsByTagName("p")[0].innerHTML="Player 1 <img src='images/victory.png'>";
+    var obj = document.getElementsByTagName("img")[0];
+    obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
+}
+else if(score1===score2){
+    document.getElementsByTagName("h1")[0].innerHTML="Its A Draw !";
+}
+else{
+    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins !";
+    document.getElementsByTagName("p")[1].innerHTML="Player 2 <img src='images/victory.png'>";
+    var obj = document.getElementsByTagName("img")[1];
+    obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
+}
+
+//Changing Dice value
+
+if(score1===1){
+        document.getElementById("d1").src="images/dice1.png";
+}
+else if(score1===2){
+    document.getElementById("d1").src="images/dice2.png";
+}
+else if(score1===3){
+    document.getElementById("d1").src="images/dice3.png";
+}
+else if(score1===4){
+    document.getElementById("d1").src="images/dice4.png";
+}
+else if(score1===5){
+    document.getElementById("d1").src="images/dice5.png";
+}
+else if(score1===6){
+    document.getElementById("d1").src="images/dice6.png";
+}
+
+if(score2===1){
+    document.getElementById("d2").src="images/dice1.png";
+}
+else if(score2===2){
+document.getElementById("d2").src="images/dice2.png";
+}
+else if(score2===3){
+document.getElementById("d2").src="images/dice3.png";
+}
+else if(score2===4){
+document.getElementById("d2").src="images/dice4.png";
+}
+else if(score2===5){
+document.getElementById("d2").src="images/dice5.png";
+}
+else if(score2===6){
+document.getElementById("d2").src="images/dice6.png";
+}
