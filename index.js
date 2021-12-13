ede22384fd9777a91d91590435706455a2a475b5
Mradul Gupta <93073227+mradul-gpt@users.noreply.github.com>
2021-12-13 17:55:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ if(score1>score2){
     var obj = document.getElementsByTagName("img")[0];
     obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
 }
-else if(score1===score2){
+if(score1===score2){
     document.getElementsByTagName("h1")[0].innerHTML="Its A Draw !";
 }
-else if(score1<score2){
+if(score1<score2){
     document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins !";
     document.getElementsByTagName("p")[1].innerHTML="Player 2 <img src='images/victory.png'>";
     var obj = document.getElementsByTagName("img")[1];
     obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
 }
+else{}
 
 //Changing Dice value
 
