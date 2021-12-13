dfae83b69331b54fafae79787ecbd0b41cd123e8
Mradul Gupta <93073227+mradul-gpt@users.noreply.github.com>
2021-12-13 17:58:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,6 @@
 var score1=Math.floor(Math.random()*7);
 var score2=Math.floor(Math.random()*7);
 
-//Genrating Results
-
-if(score1>score2){
-    document.getElementsByTagName("h1")[0].innerHTML="Player 1 Wins !";
-    document.getElementsByTagName("p")[0].innerHTML="Player 1 <img src='images/victory.png'>";
-    var obj = document.getElementsByTagName("img")[0];
-    obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
-}
-else if(score1===score2){
-    document.getElementsByTagName("h1")[0].innerHTML="Its A Draw !";
-}
-else{
-    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins !";
-    document.getElementsByTagName("p")[1].innerHTML="Player 2 <img src='images/victory.png'>";
-    var obj = document.getElementsByTagName("img")[1];
-    obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
-}
-
 //Changing Dice value
 
 if(score1===1){
@@ -62,3 +44,23 @@ else if(score2===6){
 document.getElementById("d2").src="images/dice6.png";
 }
 else{}
+
+
+//Genrating Results
+
+if(score1>score2){
+    document.getElementsByTagName("h1")[0].innerHTML="Player 1 Wins !";
+    document.getElementsByTagName("p")[0].innerHTML="Player 1 <img src='images/victory.png'>";
+    var obj = document.getElementsByTagName("img")[0];
+    obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
+}
+else if(score1===score2){
+    document.getElementsByTagName("h1")[0].innerHTML="Its A Draw !";
+}
+else{
+    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins !";
+    document.getElementsByTagName("p")[1].innerHTML="Player 2 <img src='images/victory.png'>";
+    var obj = document.getElementsByTagName("img")[1];
+    obj.setAttribute("style", "height: 50px; position: relative; bottom: 5px;");
+}
+
